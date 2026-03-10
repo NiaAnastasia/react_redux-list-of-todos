c641d01184aedc0186c8025c3a7d01f87bd7dc37
Anastasia <nastyapugach24@gmail.com>
2026-03-11 00:51:32 +0100

    task

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
+import { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useAppDispatch } from './app/hooks';
+import { getTodos } from './api';
+import { setTodos } from './features/todos';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(false);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  useEffect(() => {
+    setLoading(true);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+    getTodos()
+      .then(todos => dispatch(setTodos(todos)))
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {loading && <Loader />}
+              {!loading && <TodoList />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+      <TodoModal />
+    </>
+  );
+};
